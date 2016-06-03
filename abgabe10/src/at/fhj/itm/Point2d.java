d17bdd07abbeef987808604eef2d421b88683265
agrippa1994 <agrippa1994@icloud.com>
2016-06-03 14:34:24 +0200

    Initial commit

---
+++ abgabe10/src/at/fhj/itm/Point2d.java
@@ -0,0 +1,68 @@
+package at.fhj.itm;
+class Point2d {
+	private boolean debug;
+	protected double px;
+	protected double py;
+	
+	public Point2d (double px, double py)
+	{ 
+		this.px = px;
+		this.py = py;
+	}
+
+	public Point2d () 
+	{		
+		this(0.0, 0.0);
+	}
+
+	public Point2d (Point2d pt) 
+	{
+		this(pt.px, pt.py);
+	}
+
+	public void dprint (String s) {
+		if (debug)
+			System.out.println("Debug: " + s);
+	}
+
+	public void setDebug (boolean b) {
+		debug = true;
+	}
+
+	public void setX(double px) {
+		dprint ("setX(): Changing value of X from " + px + " to " + px );
+		this.px = px;
+	}
+
+	public double getX() {
+		return px;
+	}
+
+	public void setY(double py)  {
+		dprint ("setY(): Changing value of Y from " + py + " to " + py );
+		this.py = py;
+	}
+
+	public double getY() {
+		return py;
+	}
+
+	public void setXY(double px, double py) {
+		this.px = px;
+		this.py = py;
+	}
+
+	public double distanceFrom (Point2d pt) {
+		return Math.sqrt(Math.pow(px - pt.px, 2.0) + Math.pow(py - pt.py, 2.0));
+	}
+
+	public double distanceFromOrigin () {
+		return distanceFrom(new Point2d());
+	}
+
+	public String toString() {
+		return "Point2d [px=" + px + ", py=" + py + "]";
+	}
+	
+}
+
---
+++ abgabe10/src/at/fhj/itm/PointTester.java
@@ -0,0 +1,48 @@
+package at.fhj.itm;
+class PointTester {
+	public static void main (String[] args) {
+		Point2d  pt1 = new Point2d ();
+
+		System.out.println ("pt1 = " + pt1);
+
+		Point2d pt2 = new Point2d(4.0, 3.0);
+
+		System.out.println ("pt2 = " + pt2);
+
+		pt1.setDebug(true);	
+
+		System.out.println ("Distance from " + pt1 + " to " + pt2 +
+				" is " + pt1.distanceFrom(pt2));
+
+		System.out.println ("Distance from " + pt2 + " to " + pt1 +
+				" is " + pt2.distanceFrom(pt1));
+
+		System.out.println ("Distance from " + pt1 + " to the origin (0, 0) is " +
+				pt1.distanceFromOrigin());
+
+		System.out.println ("Distance from " + pt2 + " to the origin (0, 0) is " +
+				pt2.distanceFromOrigin());
+
+		pt1.setXY(3, 5);
+		System.out.println ("pt1 = " + pt1);
+
+		pt2.setXY(-3, -5);
+		System.out.println ("pt2 = " + pt2);
+
+		System.out.println ("Distance from " + pt1 + " to " + pt2 +
+				" is " + pt1.distanceFrom(pt2));
+
+		System.out.println ("Distance from " + pt2 + " to " + pt1 +
+				" is " + pt2.distanceFrom(pt1));
+
+		pt1.setDebug(false); 
+
+		System.out.println ("Distance from " + pt1 + " to the origin (0, 0) is " +
+				pt1.distanceFromOrigin());
+
+		System.out.println ("Distance from " + pt2 + " to the origin (0, 0) is " +
+				pt2.distanceFromOrigin());
+		
+	}
+}
+
